db7b925529cc57c12b6be465f528c6d6ccd86407
chengxiaojun <cxj920204@126.com>
2019-01-09 00:28:50 +0800

    First commit.

---
+++ app/src/main/java/com/example/coolweather/db/City.java
@@ -0,0 +1,13 @@
+package com.example.coolweather.db;
+
+
+import org.litepal.crud.LitePalSupport;
+
+public class City extends LitePalSupport {
+    private int id;
+    private String cityName;
+    private int cityCode;
+    private int provinceId;
+
+
+}
